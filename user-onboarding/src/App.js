a088f61604dbcd3c0323cb59db41bcf6cdc53a1e
theJOHNwatt <j.watt10@gmail.com>
2019-09-19 13:05:01 -0700

    :sparkles: Formik Form
    form created with multiple fields and a check box for ToS. Adding validations next with Yup

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Form from './components/Form';
+import UserForm from './components/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <UserForm/>
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,1 +1,35 @@
 import React from 'react';
+import {withFormik, Form, Field} from 'formik';
+
+
+const UserForm = ({values}) => {
+
+
+    return (
+        <div className='user-form'>
+            <Form>
+                <Field type='text' name='name' placeholder='Full Name' />
+                <Field type='text' name='email' placeholder='Email' />
+                <Field type='text' name='password' placeholder='Password' />
+                <label classeName='checkbox-container'>
+                <Field type='checkbox' name='tos' checked={values.tos} /> 
+                Terms of Service
+                </label>
+                <button>Submit</button>
+            </Form>
+        </div>
+    )
+}
+
+const FormikUserForm = withFormik ({
+    mapPropsToValues({name, email, password, tos}) {
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            tos: tos || false
+        }
+    }
+})(UserForm);
+
+export default FormikUserForm;
