39a7d0f1e9ccaf4f7fcf13f30f8e2e7ea9f13fa5
theJOHNwatt <j.watt10@gmail.com>
2019-09-19 14:48:31 -0700

    :rocket: MVP Complete
    form finished with two forms of validation and is posting to reqres. console.log is displaying all new inputs that are submitted and basic styles added to the page. All new entries display under form

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
 
-const UserForm = ({values}) => {
+const UserForm = ({values, errors, touched, status}) => {
 
+    const [user,setUser] = useState([]);
+
+    useEffect(() => {
+        if (status) {
+            setUser([...user, status]);
+        }
+      }, [status]);
 
     return (
-        <div className='user-form'>
-            <Form>
-                <Field type='text' name='name' placeholder='Full Name' />
-                <Field type='text' name='email' placeholder='Email' />
-                <Field type='text' name='password' placeholder='Password' />
-                <label classeName='checkbox-container'>
-                <Field type='checkbox' name='tos' checked={values.tos} /> 
-                Terms of Service
+        <div className='user-form-container'>
+            <Form className='user-form'>
+                <label className='form-name'>
+                <Field 
+                    type='text' 
+                    name='name' 
+                    placeholder='Full Name' 
+                />
+                {touched.name && errors.name && <p className="error">{errors.name}</p>}
+                </label>
+                <label className='form-email'>
+                <Field 
+                    type='text' 
+                    name='email' 
+                    placeholder='Email' 
+                />
+                </label>
+                <label className='form-password'>
+                <Field 
+                    type='text' 
+                    name='password' 
+                    placeholder='Password' 
+                />
+                {touched.password && errors.password && <p className="error">{errors.password}</p>}
+                </label>
+                <label className='checkbox-container'>
+                    <Field 
+                        type='checkbox' 
+                        name='tos' 
+                        checked={values.tos} 
+                    /> 
+                        Terms of Service
                 </label>
                 <button>Submit</button>
             </Form>
+            {user.map(item => (
+            <div className='returned-data' key={item.id}>
+            <p>Name: {item.name}</p>
+            <p>Email: {item.email}</p>
+            </div>   
+            ))}
         </div>
     )
 }
@@ -29,7 +68,23 @@ const FormikUserForm = withFormik ({
             password: password || '',
             tos: tos || false
         }
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("This Field is Required"),
+        password: Yup.string().required("This Field is Required")
+      }),
+
+    handleSubmit(values, { setStatus} ) {
+        axios
+        .post('https://reqres.in/api/users', values)
+        .then (res => {
+            setStatus(res.data);
+            console.log('Data Returned',res.data);
+        })
+        .catch(err => console.log('Data was not found',err.res));
     }
+    
 })(UserForm);
 
 export default FormikUserForm;
